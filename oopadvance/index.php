e13baf2aeea67f8e1680f7f52001b0aa13ca0f29
faridmia <faridcse7800@gmail.com>
2020-12-12 13:22:55 +0600

    single responsiblity principle

---
+++ oopadvance/index.php
@@ -0,0 +1,4 @@
+<?php 
+    phpinfo();
+?>
+C:\xampp\php
---
+++ oopadvance/solid/s2.php
@@ -0,0 +1,18 @@
+<?php
+
+class Student{
+
+    function checkAttendencae(){
+
+    }
+
+    function calculateGrade(){}
+
+    function collectFee(){}
+}
+
+class Gradecalculator{}
+
+class Attendence{}
+
+class StudentPayment{}
