2464e30847dd424cfa2b608e2ce26a9207d1f3d0
faridmia <faridcse7800@gmail.com>
2021-01-14 00:07:01 +0600

    nl2br line break

---
+++ class06_string/string_10.php
@@ -0,0 +1,5 @@
+<?php 
+
+$stirng = "Lorem ipsum dolor, sit \n amet consectetur adipisicing elit. \nModi, facere ipsam dolor eaque excepturi \n quae labore corrupti? Vel, rerum minima.";
+
+echo nl2br($stirng);//borwser a line break dakhanor jonno
