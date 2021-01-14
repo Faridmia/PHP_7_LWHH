2f3468c87cded0ef35c538ecaa485d29dba430a3
faridmia <faridcse7800@gmail.com>
2021-01-14 22:24:22 +0600

    file read

---
+++ filefolder/f01.php
@@ -0,0 +1,35 @@
+<?php
+$filename = 'C:\xampp\htdocs\oop\filefolder\f1.txt';//directory te double quote dile kaj kore na
+if(is_readable($filename)){
+    $fp = fopen($filename, "r");
+
+
+    $line = fgets($fp);//
+    //echo $line;
+
+    while($line = fgets($fp)){
+        echo $line;
+    }
+
+    //rewind($fp);
+    fseek($fp,5);
+    fseek($fp,-1,SEEK_END);
+
+    while($line = fgets($fp)){
+        echo $line."-";
+    }
+
+    $data = file($filename);
+
+    print_r($data);
+
+    echo "testi ng data\n";
+
+    echo $data[2];
+
+}
+
+
+
+
+
